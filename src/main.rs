c913863f417efd6be8c0f8ef35dbd5eeec95f730
cebulski.mi <cebulski.mi@gmail.com>
2025-01-31 15:04:35 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn calc_green_and_yellow(secret: &[u8], guess: &[u8]) -> String {
+    todo!()
+}
+
+
+#[test]
+fn all_wrong() {
+    assert_eq!(
+        &calc_green_and_yellow(&[5, 6, 7, 8], &[1, 2, 3, 4]),
+        "🟥🟥🟥🟥"
+    );
+}
+
+#[test]
+fn all_green() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 2, 3, 4], &[1, 2, 3, 4]),
+        "🟩🟩🟩🟩"
+    );
+}
+
+#[test]
+fn one_wrong() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 2, 3, 5], &[1, 2, 3, 4]),
+        "🟩🟩🟩🟥"
+    );
+}
+
+#[test]
+fn all_yellow() {
+    assert_eq!(
+        &calc_green_and_yellow(&[4, 3, 2, 1], &[1, 2, 3, 4]),
+        "🟨🟨🟨🟨"
+    );
+}
+
+#[test]
+fn one_wrong_but_duplicate() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 2, 3, 1], &[1, 2, 3, 4]),
+        "🟩🟩🟩🟥"
+    );
+}
+
+#[test]
+fn one_right_others_duplicate() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 1, 1, 1], &[1, 2, 3, 4]),
+        "🟩🟥🟥🟥"
+    );
+}
+
+#[test]
+fn two_right_two_swapped() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 2, 2, 2], &[2, 2, 2, 1]),
+        "🟨🟩🟩🟨"
+    );
+}
+
+#[test]
+fn two_wrong_two_swapped() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 3, 3, 2], &[2, 2, 2, 1]),
+        "🟨🟥🟥🟨"
+    );
+}
+
+#[test]
+fn a_bit_of_everything() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 9, 4, 3], &[1, 2, 3, 4]),
+        "🟩🟥🟨🟨"
+    );
+}
+
+#[test]
+fn two_in_guess_one_in_secret() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 2, 3, 3], &[3, 9, 9, 9]),
+        "🟥🟥🟨🟥"
+    );
+}
+
+#[test]
+fn two_in_secret_one_in_guess() {
+    assert_eq!(
+        &calc_green_and_yellow(&[1, 2, 3, 4], &[3, 3, 9, 9]),
+        "🟥🟥🟨🟥"
+    );
+}
